9a241eee61468ceaabcb0aa022ffc780b1d4f5b2
SaulRight <42969230+SaulRight@users.noreply.github.com>
2018-09-30 16:52:43 +0200

    challenge 8

--- js/channels.js
+++ js/channels.js
@@ -3,3 +3,5 @@ var SevenContinentsCh = {name: '#SevenContinents', createdOn: new Date('April 1,
 var KillerAppCh = {name: '#KillerApp', createdOn: new Date('April 1, 2016'), createdBy: 'minus.plus.yummy', starred: false, expiresIn: 100, messageCount: 999};
 var FirstPersonOnMarsCh = {name: '#FirstPersonOnMars', createdOn: new Date('April 1, 2016'), createdBy: 'minus.plus.yummy', starred: true, expiresIn: 100, messageCount: 999};
 var OctoberfestCh = {name: '#Octoberfest', createdOn: new Date('April 1, 2016'), createdBy: 'minus.plus.yummy', starred: false, expiresIn: 100, messageCount: 999};
+
+var channelObjContainer = [YummyCh, SevenContinentsCh, KillerAppCh, FirstPersonOnMarsCh, OctoberfestCh];
--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@ console.log("App is alive");
 // var currentChannel = eval(currentChannelName[1] + 'Ch');
 
 //current channel
-var currentChannel;
-console.log(currentChannel);
+var currentChannel = SevenContinentsCh;
 
 //adding current location
 var currentLocation = {longitude: '11°34\'31.2"', latitude: '48°09\'20.4"', what3words: 'steaming.munched.leafing'};
+
+//adding a global messag container
+var messageContainer = [];
 
 /**
  * #6 #Switcher function for the #channels name in the right app bar
@@ -37,28 +39,29 @@ function switchChannel(channelObj) {
 
     // update currentChannel
     currentChannel = channelObj;
+    console.log(currentChannel);
 }
 
 /* star/unstar function */
 function star(status) {
     var channelName =  $('#channel-name').html().split('#');
-    console.log(channelName[1]);
+    // console.log(channelName[1]);
     channelObj = eval(channelName[1] + 'Ch');
     if (status === true) { //starred
         $('#channel-star').removeClass('far');
         $('#channel-star').addClass('fas');
-        console.log(channelObj);
+        // console.log(channelObj);
     } else if (status === false) { //not starred
         $('#channel-star').removeClass('fas');
         $('#channel-star').addClass('far');
-        console.log(channelObj);
+        // console.log(channelObj);
     } else { //toggle function for the onclick listener
         $('#channel-star').toggleClass('fas');
         $('#channel-star').toggleClass('far');
         channelObj.starred = (channelObj.starred) ? (false):(true);
         $('#channels li:contains(' + channelObj.name + ')').find('i:first').toggleClass('fas');
         $('#channels li:contains(' + channelObj.name + ')').find('i:first').toggleClass('far');
-        console.log(channelObj);
+        // console.log(channelObj);
     }
 }
 
@@ -83,4 +86,55 @@ function selectTab(tabId) {
 function toggleEmojis() {
     /* $('#emojis').show(); // #show */
     $('#emojis').toggle(); // #toggle
+}
+
+function Message(messageText) {
+    this.createdBy = currentLocation.what3words;
+    this.latitude = currentLocation.latitude;
+    this.longitude = currentLocation.longitude;
+    this.createdOn = new Date();
+    this.expiresOn = new Date(this.createdOn.getTime() + 900000);
+    this.own = true;
+    this.text = messageText;
+}
+
+function sendMessage() {
+    var messageObj = new Message($('#inputMessage').val());
+    messageContainer.push(messageObj);
+    console.log(messageObj);
+    $('#messages').append(createMessageElement(messageObj));
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    $('#inputMessage').val('');
+}
+
+function createMessageElement(messageObject) {
+    var timeOptions = {weekday: 'short', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', timeZone: 'UTC'};
+    var ownMessageString = (messageObject.own) ? ' own':'';
+    return ('<div class="message'+ ownMessageString + '"><h3><a href="' + messageObject.createdBy +
+            '" target="_blank"><strong>' + messageObject.createdBy +
+            '</strong></a>' + messageObject.createdOn.toLocaleString('en-US', timeOptions) +
+            ' <em>' + Math.round((messageObject.expiresOn.getTime() - Date.now())/60000) +
+            ' min. left</em></h3><p>' + messageObject.text +
+            '</p><button>+5 min.</button></div>');
+}
+
+function listChannels() {
+    channelObjContainer.forEach(function(element) {
+        $('#channels ul').append(createChannelElement(element));
+    });
+    switchChannel(currentChannel);
+}
+
+function createChannelElement(channelObj) {
+    var channelName = channelObj.name.split('#');
+    var starClass = (channelObj.starred) ? 'fas fa-star':'far fa-star';
+    return ($('<li>').attr('onclick', 'switchChannel(' + channelName[1] + 'Ch)')
+                .html(channelObj.name)
+                .append($('<span>').attr('class', 'channel-meta')
+                    .html($('<i>').attr('class', starClass))
+                    .append($('<small>').html(channelObj.expiresIn + ' min'))
+                    .append($('<small>').html(channelObj.messageCount + ' new'))
+                    .append($('<i>').attr('class', 'fas fa-angle-right'))
+                )
+            );
 }
